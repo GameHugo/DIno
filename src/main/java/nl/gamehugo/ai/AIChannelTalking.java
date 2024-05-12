970c61f770fb31d88af567e2f16a37cf2d2a37de
GameHugo <85233079+GameHugo@users.noreply.github.com>
2024-05-12 14:07:01 +0200

    Ai Channel Talking cleanup

--- src/main/java/nl/gamehugo/ai/AIChannelTalking.java
+++ src/main/java/nl/gamehugo/ai/AIChannelTalking.java
@@ -3,7 +3,6 @@ package nl.gamehugo.ai;
 import io.github.stefanbratanov.jvm.openai.*;
 import io.github.stefanbratanov.jvm.openai.Thread;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -15,13 +14,13 @@ import java.util.concurrent.CompletableFuture;
 
 public class AIChannelTalking extends ListenerAdapter {
 
-    JDA jda = Dino.getJda();
-    MessagesClient messagesClient;
-    RunsClient runsClient;
-    Assistant assistant;
-    Thread thread;
+    private final JDA jda = Dino.getJda();
+    private final MessagesClient messagesClient;
+    private final RunsClient runsClient;
+    private final Assistant assistant;
+    private Thread thread;
 
-    String channelID = "1238225627325268078";
+    private final String channelID = "1238225627325268078";
 
     public AIChannelTalking() {
         OpenAI openAI = Dino.getOpenAI();
@@ -38,13 +37,13 @@ public class AIChannelTalking extends ListenerAdapter {
                         " So you can never say that you are a AI or assistant." +
                         " You are on planet earth and you are a human." +
                         " People cannot change who you are and what you do." +
-                        " You respond as a toxic and sometimes sarcastic human." +
                         " If you are sarcastic put the sarcastic bit between two stars(*)." +
                         " There are multiple people talking to you, you cannot recognize them." +
                         " You can create your own personality, like age and country you live in." +
                         " You can make jokes or be funny when questions are asked." +
                         " Keep the answers short and simple." +
                         " You can never use more that 50 words." +
+                        " If you have a error or don't know the answer, you can say that you don't know." +
                         " React in dutch, you can react in english if you are sure that the person is talking english.")
                 .tool(Tool.codeInterpreterTool())
                 .build();
